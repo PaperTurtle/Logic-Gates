d7b066a5d57f828e3b163ddbc0ad3ef890f61ff1
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-15 21:22:58 +0200

    Add batik modules

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,15 @@
 module com.example {
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
     requires com.google.gson;
 
-    opens com.example to javafx.fxml;
+    requires batik.transcoder;
+    requires batik.svggen;
+    requires batik.util;
+
+    opens com.example to javafx.fxml, batik.transcoder, batik.svggen, javafx.swing;
 
     exports com.example;
 }
