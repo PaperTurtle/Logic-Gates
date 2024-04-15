7eaa5586419e8b4da55beac5fbcafe4e23ee7fe4
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-15 21:37:01 +0200

    Refactor CircuitCanvas to use Pane instead of Canvas for better flexibility and functionality

--- src/main/java/com/example/CircuitCanvas.java
+++ src/main/java/com/example/CircuitCanvas.java
@@ -1,40 +1,85 @@
 package com.example;
 
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 
-public class CircuitCanvas extends Canvas {
-    private GraphicsContext gc;
-
+public class CircuitCanvas extends Pane {
     public CircuitCanvas(double width, double height) {
-        super(width, height);
-        initialize();
+        super();
+        this.setPrefSize(width, height);
+        this.setStyle("-fx-background-color: white;");
+
+        this.setOnDragOver(event -> {
+            if (event.getGestureSource() != this && event.getDragboard().hasImage()) {
+                event.acceptTransferModes(TransferMode.COPY);
+            }
+            event.consume();
+        });
+
+        // Handling drops
+        this.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasImage()) {
+                ImageView imageView = new ImageView(db.getImage());
+                double offsetX = imageView.getBoundsInLocal().getWidth() / 2;
+                double offsetY = imageView.getBoundsInLocal().getHeight() / 2;
+                imageView.setX(event.getX() - offsetX);
+                imageView.setY(event.getY() - offsetY);
+                setupDraggableGate(imageView);
+                this.getChildren().add(imageView);
+                success = true;
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
     }
 
-    private void initialize() {
-        gc = this.getGraphicsContext2D();
-        gc.setFill(Color.WHITE);
-        gc.fillRect(0, 0, getWidth(), getHeight());
+    private void setupDraggableGate(ImageView imageView) {
+        imageView.setOnMousePressed(event -> {
+            imageView.toFront();
+        });
+
+        imageView.setOnDragDetected(event -> {
+            Dragboard db = imageView.startDragAndDrop(TransferMode.COPY);
+            ClipboardContent content = new ClipboardContent();
+            content.putImage(imageView.getImage());
+            db.setContent(content);
+            db.setDragView(imageView.getImage(), imageView.getBoundsInLocal().getWidth() / 2,
+                    imageView.getBoundsInLocal().getHeight() / 2);
+            event.consume();
+        });
+
+        imageView.setOnMouseReleased(event -> {
+        });
     }
 
     public void drawGate(LogicGate gate, double x, double y) {
-        // Assume all gates are drawn as simple circles for now
-        gc.setFill(Color.BLACK);
-        gc.strokeOval(x, y, 30, 30);
-        gc.fillText(gate.getClass().getSimpleName(), x, y + 45);
+        Image image = SvgUtil.loadSvgImage(gate.getSvgFilePath());
+        ImageView imageView = new ImageView(image);
+        imageView.setX(x);
+        imageView.setY(y);
+        setupDraggableGate(imageView);
+        this.getChildren().add(imageView);
     }
 
-    public void connectGates(LogicGate output, LogicGate input, double x1, double y1, double x2, double y2) {
-        gc.setStroke(Color.BLUE);
-        gc.strokeLine(x1 + 15, y1 + 15, x2 + 15, y2 + 15);
+    public void connectGates(double x1, double y1, double x2, double y2) {
+        Line line = new Line(x1 + 15, y1 + 15, x2 + 15, y2 + 15);
+        line.setStroke(Color.BLUE);
+        this.getChildren().add(line);
     }
 
-    public void removeGate(double x, double y) {
-        gc.clearRect(x, y, 30, 30);
+    public void removeGate(ImageView gate) {
+        this.getChildren().remove(gate);
     }
 
-    public void removeConnection(double x1, double y1, double x2, double y2) {
-        gc.clearRect(x1 + 15, y1 + 15, x2 + 15 - x1, y2 + 15 - y1);
+    public void removeConnection(Line connection) {
+        this.getChildren().remove(connection);
     }
 }
