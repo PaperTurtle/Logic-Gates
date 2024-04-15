4305348db5351e0c9b8ae0937683db744a9ad92d
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-15 18:15:06 +0200

    Implement XOR gate logic in XorGate class

---
+++ src/main/java/com/example/XorGate.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class XorGate extends LogicGate {
+    public XorGate() {
+        super();
+    }
+
+    @Override
+    public boolean evaluate() {
+        int trueCount = 0;
+        for (LogicGate input : inputs) {
+            if (input.getOutput())
+                trueCount++;
+        }
+        // XOR is true only if an odd number of inputs are true.
+        return trueCount % 2 == 1;
+    }
+}
