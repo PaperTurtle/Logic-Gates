e23b84e9b0386a412dcb954a17ea2e5e793f4280
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-16 13:04:39 +0200

    Remove drag&drop functionality

--- src/main/java/com/example/CircuitCanvas.java
+++ src/main/java/com/example/CircuitCanvas.java
@@ -3,9 +3,6 @@ package com.example;
 import javafx.scene.layout.Pane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
@@ -14,50 +11,6 @@ public class CircuitCanvas extends Pane {
         super();
         this.setPrefSize(width, height);
         this.setStyle("-fx-background-color: white;");
-
-        this.setOnDragOver(event -> {
-            if (event.getGestureSource() != this && event.getDragboard().hasImage()) {
-                event.acceptTransferModes(TransferMode.COPY);
-            }
-            event.consume();
-        });
-
-        // Handling drops
-        this.setOnDragDropped(event -> {
-            Dragboard db = event.getDragboard();
-            boolean success = false;
-            if (db.hasImage()) {
-                ImageView imageView = new ImageView(db.getImage());
-                double offsetX = imageView.getBoundsInLocal().getWidth() / 2;
-                double offsetY = imageView.getBoundsInLocal().getHeight() / 2;
-                imageView.setX(event.getX() - offsetX);
-                imageView.setY(event.getY() - offsetY);
-                setupDraggableGate(imageView);
-                this.getChildren().add(imageView);
-                success = true;
-            }
-            event.setDropCompleted(success);
-            event.consume();
-        });
-    }
-
-    private void setupDraggableGate(ImageView imageView) {
-        imageView.setOnMousePressed(event -> {
-            imageView.toFront();
-        });
-
-        imageView.setOnDragDetected(event -> {
-            Dragboard db = imageView.startDragAndDrop(TransferMode.COPY);
-            ClipboardContent content = new ClipboardContent();
-            content.putImage(imageView.getImage());
-            db.setContent(content);
-            db.setDragView(imageView.getImage(), imageView.getBoundsInLocal().getWidth() / 2,
-                    imageView.getBoundsInLocal().getHeight() / 2);
-            event.consume();
-        });
-
-        imageView.setOnMouseReleased(event -> {
-        });
     }
 
     public void drawGate(LogicGate gate, double x, double y) {
@@ -65,7 +18,6 @@ public class CircuitCanvas extends Pane {
         ImageView imageView = new ImageView(image);
         imageView.setX(x);
         imageView.setY(y);
-        setupDraggableGate(imageView);
         this.getChildren().add(imageView);
     }
 
