bb0c3a6c8b8a306a27ff9d9be9ddc01ae4f1f556
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-17 17:15:35 +0200

    Create Buffer Gate

---
+++ src/main/java/com/example/BufferGate.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.util.Arrays;
+import javafx.geometry.Point2D;
+
+/**
+ * The BufferGate class extends the LogicGate class and represents a Buffer gate
+ * in
+ * a digital circuit.
+ * A Buffer gate is a digital logic gate that outputs the same value as its
+ * input.
+ * 
+ * The class constructor sets the SVG image representing the gate, the list of
+ * input points, and the output point.
+ * 
+ * The evaluate method overrides the abstract method in the LogicGate class. It
+ * checks the input and returns the same value.
+ * 
+ * @see LogicGate
+ * @author Seweryn Czabanowski
+ */
+public class BufferGate extends LogicGate {
+    /**
+     * Constructs a BufferGate object with predefined SVG image, input points, and
+     * output point.
+     */
+    public BufferGate() {
+        super("/com/example/BUFFER_ANSI_Labelled.svg",
+                Arrays.asList(new Point2D(15, 25)),
+                new Point2D(105, 25));
+    }
+
+    /**
+     * Evaluates the state of the Buffer gate based on its input.
+     * 
+     * @return the same value as the input.
+     */
+    @Override
+    public boolean evaluate() {
+        if (inputs.isEmpty())
+            return false;
+
+        return inputs.get(0).getOutput();
+    }
+}
