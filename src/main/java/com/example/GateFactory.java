41c54b321a3639ccd831adcaaef989c18b4c6690
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-17 17:16:16 +0200

    Update Text

--- src/main/java/com/example/GateFactory.java
+++ src/main/java/com/example/GateFactory.java
@@ -15,11 +15,11 @@ public class GateFactory {
      * 
      * This method takes a string representing the type of logic gate to create. The
      * type is case-insensitive.
-     * Possible values for type are: "AND", "OR", "NOT", "NAND", "NOR", "XOR",
-     * "XNOR".
+     * Possible values for type are: "AND", "OR", "NOT", "Buffer", "NAND", "NOR",
+     * "XOR", "XNOR".
      * 
      * @param type The type of gate to create. This should be one of the following:
-     *             "AND", "OR", "NOT", "NAND", "NOR", "XOR", "XNOR".
+     *             "AND", "OR", "NOT", "Buffer", "NAND", "NOR", "XOR", "XNOR".
      * @return An instance of the specified LogicGate. If the type is not
      *         recognized, this method returns null.
      */
@@ -31,6 +31,8 @@ public class GateFactory {
                 return new OrGate();
             case "NOT":
                 return new NotGate();
+            case "BUFFER":
+                return new BufferGate();
             case "NAND":
                 return new NandGate();
             case "NOR":
@@ -39,6 +41,8 @@ public class GateFactory {
                 return new XorGate();
             case "XNOR":
                 return new XnorGate();
+            case "SWITCH":
+                return new SwitchGate();
             default:
                 return null;
         }
