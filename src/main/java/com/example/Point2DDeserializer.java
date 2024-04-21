480f7758af948506a1b9d73445ca989cf076e34e
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-21 18:59:20 +0200

    Create Deserializer

---
+++ src/main/java/com/example/Point2DDeserializer.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import com.google.gson.JsonDeserializer;
+
+import java.lang.reflect.Type;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import javafx.geometry.Point2D;
+
+public class Point2DDeserializer implements JsonDeserializer<Point2D> {
+    @Override
+    public Point2D deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+            throws JsonParseException {
+        JsonObject jsonObject = json.getAsJsonObject();
+        double x = jsonObject.get("x").getAsDouble();
+        double y = jsonObject.get("y").getAsDouble();
+        return new Point2D(x, y);
+    }
+}
