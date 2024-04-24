d0d9518318aa58244467beb4c217f83514fe982f
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-24 13:01:51 +0200

    Uncomment functionality that broke earlier versions

--- src/main/java/com/paperturtle/CircuitCanvas.java
+++ src/main/java/com/paperturtle/CircuitCanvas.java
@@ -313,70 +313,66 @@ public class CircuitCanvas extends Pane {
     }
 
     private void showPropertiesDialog(LogicGate gate) {
-        // if (openContextMenu != null) {
-        // openContextMenu.hide();
-        // openContextMenu = null;
-        // }
+        if (openContextMenu != null) {
+            openContextMenu.hide();
+            openContextMenu = null;
+        }
 
-        // Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        // alert.setTitle("Gate Properties");
-        // alert.setHeaderText("Properties for " + gate.getClass().getSimpleName());
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Gate Properties");
+        alert.setHeaderText("Properties for " + gate.getClass().getSimpleName());
 
-        // TableView<Boolean[]> table = new TableView<>();
-        // List<Pair<Boolean[], Boolean>> pairList = gate.getTruthTableData();
-        // List<Boolean[]> dataList = new ArrayList<>();
-        // for (Pair<Boolean[], Boolean> pair : pairList) {
-        // Boolean[] row = new Boolean[pair.getKey().length + 1];
-        // System.arraycopy(pair.getKey(), 0, row, 0, pair.getKey().length);
-        // row[pair.getKey().length] = pair.getValue();
-        // dataList.add(row);
-        // }
+        TableView<Boolean[]> table = new TableView<>();
+        List<Pair<Boolean[], Boolean>> pairList = gate.getTruthTableData();
+        List<Boolean[]> dataList = new ArrayList<>();
+        for (Pair<Boolean[], Boolean> pair : pairList) {
+            Boolean[] row = new Boolean[pair.getKey().length + 1];
+            System.arraycopy(pair.getKey(), 0, row, 0, pair.getKey().length);
+            row[pair.getKey().length] = pair.getValue();
+            dataList.add(row);
+        }
 
-        // ObservableList<Boolean[]> data = FXCollections.observableArrayList(dataList);
-        // table.setItems(data);
+        ObservableList<Boolean[]> data = FXCollections.observableArrayList(dataList);
+        table.setItems(data);
 
-        // int numInputs = pairList.isEmpty() || pairList.get(0).getKey().length == 0 ?
-        // 0
-        // : pairList.get(0).getKey().length;
-        // for (int i = 0; i < numInputs; i++) {
-        // TableColumn<Boolean[], String> inputCol = new TableColumn<>("Input " + (i +
-        // 1));
-        // final int index = i;
-        // inputCol.setCellValueFactory(param -> new SimpleStringProperty(
-        // param.getValue().length > index ? (param.getValue()[index] ? "1" : "0") :
-        // "N/A"));
-        // table.getColumns().add(inputCol);
-        // inputCol.setPrefWidth(USE_COMPUTED_SIZE);
-        // }
+        int numInputs = pairList.isEmpty() || pairList.get(0).getKey().length == 0 ? 0
+                : pairList.get(0).getKey().length;
+        for (int i = 0; i < numInputs; i++) {
+            TableColumn<Boolean[], String> inputCol = new TableColumn<>("Input " + (i +
+                    1));
+            final int index = i;
+            inputCol.setCellValueFactory(param -> new SimpleStringProperty(
+                    param.getValue().length > index ? (param.getValue()[index] ? "1" : "0") : "N/A"));
+            table.getColumns().add(inputCol);
+            inputCol.setPrefWidth(USE_COMPUTED_SIZE);
+        }
 
-        // TableColumn<Boolean[], String> outputCol = new TableColumn<>("Output");
-        // outputCol.setCellValueFactory(param -> new SimpleStringProperty(
-        // param.getValue()[param.getValue().length - 1] ? "1" : "0"));
-        // table.getColumns().add(outputCol);
-        // outputCol.setPrefWidth(USE_COMPUTED_SIZE);
+        TableColumn<Boolean[], String> outputCol = new TableColumn<>("Output");
+        outputCol.setCellValueFactory(param -> new SimpleStringProperty(
+                param.getValue()[param.getValue().length - 1] ? "1" : "0"));
+        table.getColumns().add(outputCol);
+        outputCol.setPrefWidth(USE_COMPUTED_SIZE);
 
-        // table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-        // table.setMinWidth(Region.USE_PREF_SIZE);
-        // table.setMinHeight(Region.USE_PREF_SIZE);
+        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        table.setMinWidth(Region.USE_PREF_SIZE);
+        table.setMinHeight(Region.USE_PREF_SIZE);
 
-        // double rowHeight = 30.0;
-        // double headerHeight = 27.0;
+        double rowHeight = 30.0;
+        double headerHeight = 27.0;
 
-        // table.prefHeightProperty().bind(Bindings.size(table.getItems()).multiply(rowHeight).add(headerHeight));
-        // table.maxHeightProperty().bind(table.prefHeightProperty());
+        table.prefHeightProperty().bind(Bindings.size(table.getItems()).multiply(rowHeight).add(headerHeight));
+        table.maxHeightProperty().bind(table.prefHeightProperty());
 
-        // table.skinProperty().addListener((obs, oldSkin, newSkin) -> {
-        // final TableHeaderRow header = (TableHeaderRow)
-        // table.lookup("TableHeaderRow");
-        // header.reorderingProperty().addListener((o, oldVal, newVal) ->
-        // header.setReordering(false));
-        // });
+        table.skinProperty().addListener((obs, oldSkin, newSkin) -> {
+            final TableHeaderRow header = (TableHeaderRow) table.lookup("TableHeaderRow");
+            header.reorderingProperty().addListener((o, oldVal, newVal) -> header.setReordering(false));
+        });
 
-        // alert.getDialogPane().setContent(table);
-        // alert.getDialogPane().getStylesheets()
-        // .add(getClass().getResource("/com/paperturtle/styles.css").toExternalForm());
-        // table.getStylesheets().add(getClass().getResource("/com/paperturtle/styles.css").toExternalForm());
-        // alert.showAndWait();
+        alert.getDialogPane().setContent(table);
+        alert.getDialogPane().getStylesheets()
+                .add(getClass().getResource("/com/paperturtle/styles.css").toExternalForm());
+        table.getStylesheets().add(getClass().getResource("/com/paperturtle/styles.css").toExternalForm());
+        alert.showAndWait();
     }
 
     public void removeGate(ImageView gate) {
