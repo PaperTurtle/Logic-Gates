528db60031af066dda86d04d25fea3f14509a893
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-30 12:30:37 +0200

    Create Clipboard data

---
+++ src/main/java/com/paperturtle/ClipboardData.java
@@ -0,0 +1,21 @@
+package com.paperturtle;
+
+import javafx.geometry.Point2D;
+
+public class ClipboardData {
+    String type;
+    Point2D position;
+
+    public ClipboardData(String type, Point2D position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Point2D getPosition() {
+        return position;
+    }
+}
