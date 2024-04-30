10acde8d43993d5ad3b9ce5d5896752ede84f354
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-04-30 12:55:33 +0200

    Add connection data

--- src/main/java/com/paperturtle/ClipboardData.java
+++ src/main/java/com/paperturtle/ClipboardData.java
@@ -1,14 +1,34 @@
 package com.paperturtle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.geometry.Point2D;
 
 public class ClipboardData {
     String type;
     Point2D position;
+    List<ConnectionDetail> connections;
+
+    public static class ConnectionDetail {
+        public String sourceId;
+        public String targetId;
+        public int sourcePointIndex;
+        public int targetPointIndex;
+
+        public ConnectionDetail(String sourceId, String targetId, int sourcePointIndex, int targetPointIndex) {
+            this.sourceId = sourceId;
+            this.targetId = targetId;
+            this.sourcePointIndex = sourcePointIndex;
+            this.targetPointIndex = targetPointIndex;
+        }
+
+    }
 
     public ClipboardData(String type, Point2D position) {
         this.type = type;
         this.position = position;
+        this.connections = new ArrayList<>();
     }
 
     public String getType() {
@@ -17,5 +37,9 @@ public class ClipboardData {
 
     public Point2D getPosition() {
         return position;
+    }
+
+    public List<ConnectionDetail> getConnections() {
+        return connections;
     }
 }
