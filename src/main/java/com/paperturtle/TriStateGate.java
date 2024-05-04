a3cec58a6618b61e6fe7dd12b6f7a62ef5d2ad0f
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-05-04 11:41:17 +0200

    Add TriState gate

--- src/main/java/com/paperturtle/TriStateGate.java
+++ src/main/java/com/paperturtle/TriStateGate.java
@@ -7,22 +7,38 @@ import javafx.geometry.Point2D;
 import javafx.util.Pair;
 
 public class TriStateGate extends LogicGate {
+    private boolean enable;
+
     public TriStateGate() {
         super("/com/paperturtle/TRISTATE_ANSI_Labelled.svg",
-                Arrays.asList(new Point2D(15, 15), new Point2D(15, 35)),
+                Arrays.asList(new Point2D(15, 25), new Point2D(66, 0)),
                 new Point2D(105, 25));
+        this.enable = false;
     }
 
     @Override
     public boolean evaluate() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'evaluate'");
+        if (inputs.size() < 2) {
+            return false;
+        }
+        LogicGate enableGate = inputs.get(1);
+        enable = enableGate.getOutput();
+
+        if (!enable) {
+            return false;
+        }
+
+        LogicGate inputGate = inputs.get(0);
+        return inputGate.getOutput();
     }
 
     @Override
     public List<Pair<Boolean[], Boolean>> getTruthTableData() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTruthTableData'");
+        return Arrays.asList(
+                new Pair<>(new Boolean[] { false, false }, false),
+                new Pair<>(new Boolean[] { false, true }, false),
+                new Pair<>(new Boolean[] { true, false }, false),
+                new Pair<>(new Boolean[] { true, true }, true));
     }
 
 }
