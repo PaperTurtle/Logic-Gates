95a8c2ab5454424ebf9600b7c8451beeddf7b979
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-05-06 18:00:18 +0200

    Add common interface for saving/loading data

--- src/main/java/com/paperturtle/CircuitFileManager.java
+++ src/main/java/com/paperturtle/CircuitFileManager.java
@@ -25,6 +25,9 @@ public class CircuitFileManager {
         this.gson = new GsonBuilder()
                 .registerTypeAdapter(Point2D.class, new Point2DSerializer())
                 .registerTypeAdapter(Point2D.class, new Point2DDeserializer())
+                .registerTypeAdapter(TextLabel.class, new TextLabelSerializer())
+                .registerTypeAdapter(TextLabel.class, new TextLabelDeserializer())
+                .registerTypeAdapter(CircuitComponent.class, new ComponentAdapter())
                 .create();
     }
 
@@ -35,10 +38,10 @@ public class CircuitFileManager {
      * @param gates The list of LogicGates to save.
      * @throws IOException If an I/O error occurs.
      */
-    public void saveCircuit(String file, List<GateData> gates) throws IOException {
-        Type type = new TypeToken<List<GateData>>() {
+    public void saveCircuit(String file, List<CircuitComponent> components) throws IOException {
+        Type type = new TypeToken<List<CircuitComponent>>() {
         }.getType();
-        String json = gson.toJson(gates, type);
+        String json = gson.toJson(components, type);
         try (FileWriter writer = new FileWriter(file)) {
             writer.write(json);
         }
@@ -51,24 +54,11 @@ public class CircuitFileManager {
      * @return A list of LogicGates reconstructed from the saved data.
      * @throws IOException If an I/O error occurs.
      */
-    public List<GateData> loadCircuit(String filePath) throws IOException {
-        Type type = new TypeToken<List<GateData>>() {
+    public List<CircuitComponent> loadCircuit(String filePath) throws IOException {
+        Type type = new TypeToken<List<CircuitComponent>>() {
         }.getType();
         try (FileReader reader = new FileReader(filePath)) {
-            JsonElement fileElement = JsonParser.parseReader(reader);
-            if (!fileElement.isJsonArray()) {
-                throw new IllegalArgumentException("Invalid JSON: Expected an array of gate data.");
-            }
-            JsonArray jsonArray = fileElement.getAsJsonArray();
-            List<GateData> gatesData = new ArrayList<>();
-
-            for (JsonElement element : jsonArray) {
-                if (!isValidGateNode(element.getAsJsonObject())) {
-                    throw new IllegalArgumentException("Invalid gate data in JSON.");
-                }
-                gatesData.add(gson.fromJson(element, GateData.class));
-            }
-            return gatesData;
+            return gson.fromJson(reader, type);
         }
     }
 
