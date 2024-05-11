1fe5e0b07b144d92e654e810d9914fbff9443ca3
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-05-11 18:22:00 +0200

    Refactor code for improved performance and maintainability

--- src/main/java/com/paperturtle/CircuitCanvas.java
+++ src/main/java/com/paperturtle/CircuitCanvas.java
@@ -11,44 +11,36 @@ import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.paperturtle.GateData.ConnectionData;
 
 public class CircuitCanvas extends Pane {
     private Line currentLine;
-    private LogicGate startGate;
     private Map<ImageView, List<Circle>> gateMarkers = new HashMap<>();
     private Map<ImageView, LogicGate> gateImageViews = new HashMap<>();
     private Point2D lastMouseCoordinates;
     private ScrollPane scrollPane;
     private Map<Line, LogicGate> lineToStartGateMap = new HashMap<>();
-    private LogicGate highlightedGate = null;
     private ContextMenu openContextMenu = null;
     private Set<LogicGate> gatesToBeUpdated = new HashSet<>();
-    private Rectangle selectionRect = new Rectangle();
-    private boolean isSelecting = false;
-    private boolean justSelected = false;
     private double lastX = 0;
     private double lastY = 0;
     private Point2D virtualOrigin = new Point2D(0, 0);
     private CursorMode currentCursorMode = CursorMode.POINTER;
-    private List<ClipboardData> clipboard = new ArrayList<>();
     private InteractionManager interactionManager;
     private ConnectionManager connectionManager;
     private GateManager gateManager;
+    private ClipboardManager clipboardManager;
 
     public enum CursorMode {
         POINTER, GRABBY
@@ -64,6 +56,7 @@ public class CircuitCanvas extends Pane {
         this.interactionManager = new InteractionManager(this);
         this.connectionManager = new ConnectionManager(this);
         this.gateManager = new GateManager(this);
+        this.clipboardManager = new ClipboardManager(this);
 
         interactionManager.initializeSelectionMechanism();
 
@@ -72,96 +65,16 @@ public class CircuitCanvas extends Pane {
 
         this.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.C && event.isControlDown()) {
-                copySelectedGatesToClipboard();
+                clipboardManager.copySelectedGatesToClipboard();
                 event.consume();
             } else if (event.getCode() == KeyCode.V && event.isControlDown()) {
-                pasteGatesFromClipboard();
+                clipboardManager.pasteGatesFromClipboard();
                 event.consume();
             } else if (event.getCode() == KeyCode.X && event.isControlDown()) {
-                removeSelectedGates();
+                clipboardManager.removeSelectedGates();
                 event.consume();
             }
         });
-    }
-
-    private void pasteGatesFromClipboard() {
-        deselectAllGates();
-        Map<String, LogicGate> createdGates = new HashMap<>();
-        double offsetX = 30;
-        double offsetY = 30;
-
-        for (ClipboardData data : clipboard) {
-            LogicGate gate = GateFactory.createGate(normalizeType(data.getType()));
-            if (gate == null) {
-                System.out.println("Unable to create gate of type: " + data.getType());
-                continue;
-            }
-
-            double newX = data.getPosition().getX() + offsetX;
-            double newY = data.getPosition().getY() + offsetY;
-
-            gate.setPosition(newX, newY);
-            gate.setId(data.getId());
-            createdGates.put(data.getId(), gate);
-            drawGate(gate, newX, newY);
-            gate.getImageView().getStyleClass().add("selected");
-        }
-
-        for (ClipboardData data : clipboard) {
-            LogicGate sourceGate = createdGates.get(data.getId());
-            if (sourceGate == null)
-                continue;
-
-            for (ClipboardData.ConnectionData output : data.getOutputs()) {
-                LogicGate targetGate = createdGates.get(output.gateId);
-                if (targetGate == null) {
-                    System.out.println("Output gate not found for ID: " + output.gateId);
-                    continue;
-                }
-                Point2D sourcePos = sourceGate.getOutputMarker().localToParent(
-                        sourceGate.getOutputMarker().getCenterX(), sourceGate.getOutputMarker().getCenterY());
-                Point2D targetPos = targetGate.getInputMarkers().get(output.pointIndex).localToParent(
-                        targetGate.getInputMarkers().get(output.pointIndex).getCenterX(),
-                        targetGate.getInputMarkers().get(output.pointIndex).getCenterY());
-
-                Line connectionLine = new Line(sourcePos.getX(), sourcePos.getY(), targetPos.getX(), targetPos.getY());
-                connectionLine.setStrokeWidth(3.5);
-                connectionLine.setStroke(Color.BLACK);
-
-                this.getChildren().add(connectionLine);
-                sourceGate.addOutputConnection(connectionLine);
-                targetGate.addInputConnection(connectionLine, output.pointIndex);
-            }
-        }
-    }
-
-    private void copySelectedGatesToClipboard() {
-        clipboard.clear();
-        gateImageViews.entrySet().stream()
-                .filter(entry -> entry.getKey().getStyleClass().contains("selected"))
-                .forEach(entry -> {
-                    LogicGate gate = entry.getValue();
-                    clipboard.add(gate.getGateClipboardData());
-                });
-    }
-
-    private void deselectAllGates() {
-        gateImageViews.values().forEach(gate -> {
-            gate.getImageView().getStyleClass().remove("selected");
-            if (gate instanceof SwitchGate) {
-                ((SwitchGate) gate).setSelected(false);
-            }
-        });
-        highlightedGate = null;
-    }
-
-    private void removeSelectedGates() {
-        List<ImageView> selectedGates = gateImageViews.entrySet().stream()
-                .filter(entry -> entry.getKey().getStyleClass().contains("selected"))
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
-
-        selectedGates.forEach(gateManager::removeGate);
     }
 
     public void drawGate(LogicGate gate, double x, double y) {
@@ -179,24 +92,6 @@ public class CircuitCanvas extends Pane {
         textLabel.setLayoutX(x);
         textLabel.setLayoutY(y);
         interactionManager.setupDragHandlersForLabel(textLabel);
-    }
-
-    public void setupOutputInteraction(Circle outputMarker, LogicGate gate) {
-        outputMarker.setOnMouseClicked(event -> {
-            if (event.getButton() == MouseButton.PRIMARY && currentLine == null) {
-                Point2D outputPos = outputMarker.localToParent(outputMarker.getCenterX(), outputMarker.getCenterY());
-                currentLine = new Line(outputPos.getX(), outputPos.getY(), event.getX(), event.getY());
-                Color lineColor = gate.evaluate() ? Color.RED : Color.BLACK;
-                currentLine.setStroke(lineColor);
-                currentLine.setStrokeWidth(3.5);
-                this.getChildren().add(currentLine);
-                lineToStartGateMap.put(currentLine, gate);
-                startGate = gate;
-                gate.addOutputConnection(currentLine);
-                interactionManager.setupConnectionHandlers();
-                event.consume();
-            }
-        });
     }
 
     public void scheduleUpdate(LogicGate gate) {
@@ -287,7 +182,7 @@ public class CircuitCanvas extends Pane {
         }
     }
 
-    private String normalizeType(String type) {
+    public String normalizeType(String type) {
         if (type.endsWith("Gate")) {
             type = type.substring(0, type.length() - 4);
         }
@@ -356,32 +251,12 @@ public class CircuitCanvas extends Pane {
         return virtualOrigin;
     }
 
-    public boolean isSelecting() {
-        return isSelecting;
-    }
-
-    public void setSelecting(boolean selecting) {
-        isSelecting = selecting;
-    }
-
     public Map<ImageView, LogicGate> getGateImageViews() {
         return gateImageViews;
     }
 
-    public LogicGate getHighlightedGate() {
-        return highlightedGate;
-    }
-
-    public void setHighlightedGate(LogicGate gate) {
-        this.highlightedGate = gate;
-    }
-
     public CursorMode getCurrentCursorMode() {
         return currentCursorMode;
-    }
-
-    public Rectangle getSelectionRect() {
-        return selectionRect;
     }
 
     public ContextMenu getOpenContextMenu() {
@@ -390,14 +265,6 @@ public class CircuitCanvas extends Pane {
 
     public void setOpenContextMenu(ContextMenu openContextMenu) {
         this.openContextMenu = openContextMenu;
-    }
-
-    public boolean isJustSelected() {
-        return justSelected;
-    }
-
-    public void setJustSelected(boolean justSelected) {
-        this.justSelected = justSelected;
     }
 
     public Line getCurrentLine() {
@@ -410,14 +277,6 @@ public class CircuitCanvas extends Pane {
 
     public Map<ImageView, List<Circle>> getGateMarkers() {
         return gateMarkers;
-    }
-
-    public LogicGate getStartGate() {
-        return startGate;
-    }
-
-    public void setStartGate(LogicGate startGate) {
-        this.startGate = startGate;
     }
 
     public Map<Line, LogicGate> getLineToStartGateMap() {
