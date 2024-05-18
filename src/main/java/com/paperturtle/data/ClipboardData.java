3205492d29b2bbd4a8625f27d848f4950aae1f30
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-05-18 18:47:13 +0200

    Add docs for data

--- src/main/java/com/paperturtle/data/ClipboardData.java
+++ src/main/java/com/paperturtle/data/ClipboardData.java
@@ -3,8 +3,21 @@ package com.paperturtle.data;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.paperturtle.commands.Command;
+import com.paperturtle.commands.PasteGatesCommand;
+
 import javafx.geometry.Point2D;
 
+/**
+ * Represents the clipboard data for logic gates and their connections.
+ * Used for copying and pasting gates within the circuit canvas.
+ * 
+ * @see Command
+ * @see ConnectionData
+ * @see PasteGatesCommand
+ * 
+ * @author Seweryn Czabanowski
+ */
 public class ClipboardData {
     public String id;
     public String type;
@@ -13,10 +26,20 @@ public class ClipboardData {
     public List<ConnectionData> inputs = new ArrayList<>();
     public List<ConnectionData> outputs = new ArrayList<>();
 
+    /**
+     * Represents a connection data between gates.
+     * Stores the ID of the gate and the index of the connection point.
+     */
     public static class ConnectionData {
         public String gateId;
         public int pointIndex;
 
+        /**
+         * Constructs a ConnectionData with the specified gate ID and point index.
+         * 
+         * @param gateId     the ID of the connected gate
+         * @param pointIndex the index of the connection point
+         */
         public ConnectionData(String gateId, int pointIndex) {
             this.gateId = gateId;
             this.pointIndex = pointIndex;
@@ -24,26 +47,56 @@ public class ClipboardData {
 
     }
 
+    /**
+     * Gets the ID of the gate.
+     * 
+     * @return the ID of the gate
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the type of the gate.
+     * 
+     * @return the type of the gate
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Gets the position of the gate.
+     * 
+     * @return the position of the gate
+     */
     public Point2D getPosition() {
         return position;
     }
 
+    /**
+     * Gets the state of the gate.
+     * 
+     * @return the state of the gate
+     */
     public boolean getState() {
         return state;
     }
 
+    /**
+     * Gets the list of input connections.
+     * 
+     * @return the list of input connections
+     */
     public List<ConnectionData> getInputs() {
         return inputs;
     }
 
+    /**
+     * Gets the list of output connections.
+     * 
+     * @return the list of output connections
+     */
     public List<ConnectionData> getOutputs() {
         return outputs;
     }
--- src/main/java/com/paperturtle/data/GateData.java
+++ src/main/java/com/paperturtle/data/GateData.java
@@ -6,6 +6,14 @@ import java.util.List;
 
 import com.paperturtle.utils.CircuitComponent;
 
+/**
+ * Represents the data for a logic gate including its connections.
+ * Implements the CircuitComponent interface.
+ * 
+ * @see CircuitComponent
+ * 
+ * @author Seweryn Czabanowski
+ */
 public class GateData implements CircuitComponent {
     public String id;
     public String type;
@@ -14,16 +22,31 @@ public class GateData implements CircuitComponent {
     public List<ConnectionData> inputs = new ArrayList<>();
     public List<ConnectionData> outputs = new ArrayList<>();
 
+    /**
+     * Represents a connection data between gates.
+     * Stores the ID of the gate and the index of the connection point.
+     */
     public static class ConnectionData {
         public String gateId;
         public int pointIndex;
 
+        /**
+         * Constructs a ConnectionData with the specified gate ID and point index.
+         * 
+         * @param gateId     the ID of the connected gate
+         * @param pointIndex the index of the connection point
+         */
         public ConnectionData(String gateId, int pointIndex) {
             this.gateId = gateId;
             this.pointIndex = pointIndex;
         }
     }
 
+    /**
+     * Gets the component type.
+     * 
+     * @return the component type as a string
+     */
     @Override
     public String getComponentType() {
         return "gate";
