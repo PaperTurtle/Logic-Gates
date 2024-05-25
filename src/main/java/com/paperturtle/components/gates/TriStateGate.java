54fe3487e8290163c6319eb633d619fbc1b5060e
Seweryn Czabanowski <seweryn_cz@outlook.com>
2024-05-25 14:12:49 +0200

    Update TriState

--- src/main/java/com/paperturtle/components/gates/TriStateGate.java
+++ src/main/java/com/paperturtle/components/gates/TriStateGate.java
@@ -23,6 +23,11 @@ public class TriStateGate extends LogicGate {
     private boolean enable;
 
     /**
+     * Represents the high-impedance state of the TriState gate.
+     */
+    private boolean highImpedance;
+
+    /**
      * Constructs a TriStateGate object with predefined SVG image, input points, and
      * output point.
      */
@@ -31,20 +36,25 @@ public class TriStateGate extends LogicGate {
                 Arrays.asList(new Point2D(15, 25), new Point2D(66, 0)),
                 new Point2D(105, 25));
         this.enable = false;
+        this.highImpedance = true;
     }
 
     @Override
     public boolean evaluate() {
         if (inputs.size() < 2) {
+            highImpedance = true;
             return false;
         }
+
         LogicGate enableGate = inputs.get(1);
         enable = enableGate.getOutput();
 
         if (!enable) {
+            highImpedance = true;
             return false;
         }
 
+        highImpedance = false;
         LogicGate inputGate = inputs.get(0);
         return inputGate.getOutput();
     }
@@ -56,6 +66,15 @@ public class TriStateGate extends LogicGate {
                 new Pair<>(new Boolean[] { false, true }, false),
                 new Pair<>(new Boolean[] { true, false }, false),
                 new Pair<>(new Boolean[] { true, true }, true));
+    }
+
+    /**
+     * Checks if the gate is in a high-impedance state.
+     * 
+     * @return true if the gate is in high-impedance state, false otherwise.
+     */
+    public boolean isHighImpedance() {
+        return highImpedance;
     }
 
 }
